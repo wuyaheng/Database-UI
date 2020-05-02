0d8b3412fb34461a79a8b42c9139902f3c9e6ad7
wuyaheng <wuyaheng2016@gmail.com>
2020-05-02 01:10:52 -0400

    solve bug

--- app/config/connection.js
+++ app/config/connection.js
@@ -5,6 +5,7 @@ var sequelize = process.env.JAWSDB_URL
                 ? new Sequelize(process.env.j4b8wlcmhefl8ha3, process.env.f9c4oz4nl6no6qhv, process.env.alwdfwp5rxbgs1hw, {
                   host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
                   port: 3306,
+                  dialect: "mysql",
                   pool: {
                     max: 5,
                     min: 0,
