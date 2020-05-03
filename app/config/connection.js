ebf29cdf9446a1ea367ae2bebbef1752a4ba3a57
wuyaheng <wuyaheng2016@gmail.com>
2020-05-02 23:45:22 -0400

    upload files

--- app/config/connection.js
+++ app/config/connection.js
@@ -12,7 +12,7 @@ var sequelize = process.env.JAWSDB_URL
                     idle: 10000
                   }
                 })
-                : new Sequelize("company_db", "root", "Diallo123_", {
+                : new Sequelize("company_db", "root", "", {
                   host: "localhost",
                   port: 3306,
                   dialect: "mysql",
