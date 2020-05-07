ae0e5e27b1938c19ba3713c964c27695cb8791aa
wuyaheng <wuyaheng2016@gmail.com>
2020-05-07 00:50:01 -0400

    upload files

--- model/engagement.js
+++ model/engagement.js
@@ -1,5 +1,8 @@
 var Sequelize = require("sequelize");
 var sequelize = require("../app/config/connection");
+var Service = require("./service");
+var Employee = require("./employee");
+var Client = require("./client");
 
 
 var Engagement = sequelize.define("engagement", {
@@ -12,6 +15,11 @@ var Engagement = sequelize.define("engagement", {
   ClientID: Sequelize.STRING,
   Status: Sequelize.STRING 
 });
+
+// Engagement.hasOne(Client, { foreignKey: 'ClientID' });
+// Engagement.belongsTo(Service, { foreignKey: 'ServiceID' });
+// Engagement.belongsToMany(Employee, { foreignKey: 'EmployeeID' });
+
 
 // Syncs with DB
 Engagement.sync({ alter: true });
